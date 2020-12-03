29f1f4056a029eecf2c4f97d18ba05b90be662a1
blen_meku <mekuriablen@gmail.com>
2020-12-02 21:56:56 -0800

    commit tree e45b27a077fc1dd722e20146fc0340d3d94e45d6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(arr, fn) {
+    let r = []
+    for (let i = 0; i < arr.length; i++) {
+        r.push(fn(arr[i]))
+    }
+    return r
+}
+
+function reduce(src, cb, startingPoint) {
+  let r = (!!startingPoint) ? startingPoint: src[0]
+    let i = (!!startingPoint) ? 0 : 1
+    for (; i < src.length; i++) {
+        r = cb(src[i], r)
+    }
+    return r;
+} 
